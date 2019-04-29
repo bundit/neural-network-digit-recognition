5c94037ddacd48b76ed315793878caf0d8ffca4b
Bundit Hongmanee <bundit.j.h@gmail.com>
2019-04-29 12:51:26 -0700

    Added NetLayer testing. Working appropriately

--- NetLayer.cpp
+++ NetLayer.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+//#include "stdafx.h"
 #include "NetLayer.h"
 #include "MatrixManipulation.h"
 #include <iostream>
@@ -22,22 +22,28 @@ NetLayer::NetLayer(int iInput, int iOutput) {
 	//MM::printMatrix(this->weights);
 
 }
-//public functions
+
+//\/\/\/\/\/\/\/\/\/\/
+// public functions //
+//\/\/\/\/\/\/\/\/\/\/
 
 //Compute the matrix multiplication and apply the sigmoid function to the output
 Matrix<double> NetLayer::computeOutput() {
 	//matrix multiplication 
-	//X = W*I
+	//X = W * I
 	
 	using namespace std;
 	this->outputs = this->weights*this->inputs; //the work
 	
 	//save output before applying sigmoid for back propagation
-	this->beforeSig = outputs; 
+//    this->setOriginalOutput(outputs);
+	this->beforeSig = this->outputs;
 
 	//apply activation function to the matrix
 	activation(this->outputs);
-	
+    
+    
+//    this->setOutput()
 	return this->outputs;
 }
 
@@ -57,7 +63,9 @@ double NetLayer::sigmoid(double x) {
 	return y;
 }
 
-//private functions
+//\/\/\/\/\/\/\/\/\/\//
+// private functions //
+//\/\/\/\/\/\/\/\/\/\//
 
 //Generate and assign random weights for this layer
 //Given a range from -1/sqrt(numInput) to +1/sqrt(numInput)
@@ -103,25 +111,80 @@ Matrix<double> NetLayer::getOriginalOutput() {
 	return this->beforeSig;
 }
 
-/*
-int main() {
-	NetLayer n;
-	Matrix<double> inputs;
-	inputs.push_back({ 1.0 });
-	inputs.push_back({ 0.5 });
 
-	Matrix<double> weights;
-	weights.push_back({ 0.9, 0.3 });
-	weights.push_back({ 0.2, 0.8 });
+int main() {
+    
+    // Testing case 1
+    /////////////////
+	NetLayer n;
+    Matrix<double> inputs{ {1.0}, {0.5} };
+    Matrix<double> weights{ {0.9, 0.3}, {0.2, 0.8}};
 
 	n.setInput(inputs);
-	n.setWeights(weights);
-
-	MM::printMatrix(inputs);
+    n.setWeights(weights);
+    
+    std::cout << "Case #1 -Input and weights before computation-" << std::endl;
+    std::cout << "Input vector:" << std::endl;
+	MM::printMatrix(n.getInputs());
 	std::cout << std::endl;
-	MM::printMatrix(weights);
+    std::cout << "Weight matrix:" << std::endl;
+	MM::printMatrix(n.getWeights());
 	std::cout << std::endl;
 
-	MM::printMatrix(n.computeOutput());
+    std::cout << "Case #1 -Outputs after computation-" << std::endl;
+    n.computeOutput();
+    std::cout << "Original output before sigmoid function: " << std::endl;
+    MM::printMatrix(n.getOriginalOutput());
+    std::cout << std::endl;
+    std::cout << "Output after sigmoid function: " << std::endl;
+    MM::printMatrix(n.getOutput());
+    
+    // Testing case 2
+    /////////////////
+    NetLayer n2;
+    Matrix<double> i{ {0.9},{0.1},{0.8} };
+    Matrix<double> w{ {0.9,0.3,0.4}, {0.2,0.8,0.2}, {0.1,0.5,0.6} };
+    n2.setInput(i);
+    n2.setWeights(w);
+    
+    std::cout << "Case #2 -Input and weights before computation-" << std::endl;
+    std::cout << "Input vector:" << std::endl;
+    MM::printMatrix(n2.getInputs());
+    std::cout << std::endl;
+    std::cout << "Weight matrix:" << std::endl;
+    MM::printMatrix(n2.getWeights());
+    std::cout << std::endl;
+    
+    std::cout << "Case #2 -Outputs after computation-" << std::endl;
+    n2.computeOutput();
+    std::cout << "Original output before sigmoid function: " << std::endl;
+    MM::printMatrix(n2.getOriginalOutput());
+    std::cout << std::endl;
+    std::cout << "Output after sigmoid function: " << std::endl;
+    MM::printMatrix(n2.getOutput());
+    
+    // Testing case 3
+    /////////////////
+    NetLayer n3;
+    Matrix<double> i2 = n2.getOutput();
+    Matrix<double> w2{ {0.3,0.7,0.5}, {0.6,0.5,0.2}, {0.8,0.1,0.9} };
+    n3.setInput(i2);
+    n3.setWeights(w2);
+    
+    std::cout << "Case #3 -Input and weights before computation-" << std::endl;
+    std::cout << "Input vector:" << std::endl;
+    MM::printMatrix(n3.getInputs());
+    std::cout << std::endl;
+    std::cout << "Weight matrix:" << std::endl;
+    MM::printMatrix(n3.getWeights());
+    std::cout << std::endl;
+    
+    std::cout << "Case #2 -Outputs after computation-" << std::endl;
+    n3.computeOutput();
+    std::cout << "Original output before sigmoid function: " << std::endl;
+    MM::printMatrix(n3.getOriginalOutput());
+    std::cout << std::endl;
+    std::cout << "Output after sigmoid function: " << std::endl;
+    MM::printMatrix(n3.getOutput());
 }
-*/
+
